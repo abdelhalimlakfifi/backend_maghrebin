83fa95439eff3471472fec418879f84ffac2c74d
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-24 18:53:03 +0100

    create permissions model file

---
+++ models/permissionModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const permessionSchema = new mongoose.Schema({
+    label:{
+        type: String,
+        required: true
+    }, 
+    timestamps: true 
+})
+
+
+const Permission = mongoose.model('Permission', permessionSchema,'permissions')
+
+
+module.exports = Permission;

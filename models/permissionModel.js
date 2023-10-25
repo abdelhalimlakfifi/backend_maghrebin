c83591f1bde302d05f80800714dccf26d089dfdf
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-25 10:17:54 +0100

    correct timesstamps

--- models/permissionModel.js
+++ models/permissionModel.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const permessionSchema = new mongoose.Schema({
-    label:{
-        type: String,
-        required: true
-    }, 
-    timestamps: true 
-})
+const permessionSchema = new mongoose.Schema(
+    {
+        label:{
+            type: String,
+            required: true
+        }, 
+    },
+    {timestamps: true }
+)
 
 
 const Permission = mongoose.model('Permission', permessionSchema,'permissions')

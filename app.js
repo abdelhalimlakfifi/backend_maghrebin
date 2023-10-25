00486c568435aa1982602eadf182722b7c23fa6f
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-25 13:26:56 +0100

    adding json middleware

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
-const roleRoute = require('./routes/roles.route')
+const roleRoute = require('./routes/backoffice/roles.route')
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
 

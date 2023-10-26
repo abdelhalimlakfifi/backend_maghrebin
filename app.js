953abf94e04961952b17f3d7a9b3867ccd36cabd
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-26 10:36:07 +0100

    import categorie route

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
 const morgan = require('morgan');
-const roleRoute = require('./routes/backoffice/roles.route')
 
+//routes
+const roleRoute = require('./routes/backoffice/roles.route')
+const categorieRoute = require('./routes/backoffice/categorie.route');
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -18,6 +20,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 app.use('/api/role', roleRoute);
+app.use('/api/categorie', categorieRoute);
 
 
 const PORT = process.env.PORT || 3000;

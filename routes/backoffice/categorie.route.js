45df80c9d6086a7bd39041240f322136df1a47e9
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-10-27 14:45:51 +0100

    add getone search update route

--- routes/backoffice/categorie.route.js
+++ routes/backoffice/categorie.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const categorieController = require('../../controllers/backoffice/categorie.controller');
 const { authenticateToken } = require('../../middleware/authMiddleware');
-const passport = require('passport');
 const { categorieMiddleware } = require('../../middleware/backoffice/categorie.middleware');
 
 router.get('/', authenticateToken, categorieMiddleware('categorie-read'), categorieController.index);
 router.post('/store', authenticateToken, categorieMiddleware('categorie-read'), categorieController.storingValidation , categorieController.store);
-
+router.get('/getone/:categoriename',authenticateToken, categorieMiddleware('categorie-read'), categorieController.getOne);
+router.get('/search/:search',authenticateToken, categorieMiddleware('categorie-read'), categorieController.search);
+router.put('/update/:id',authenticateToken, categorieMiddleware('categorie-edit'),categorieController.storingValidation, categorieController.update);
 module.exports=router;

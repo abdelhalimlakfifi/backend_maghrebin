43e835aeb4fcd73782f03ae32d406053bc730766
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-10-27 12:21:05 +0100

    create getting all

--- controllers/backoffice/categorie.controller.js
+++ controllers/backoffice/categorie.controller.js
@@ -1,15 +1,52 @@
 const Categorie = require('../../models/categorie.model');
+const { internalError } = require('../../utils/500');
+const { body, validationResult} = require('express-validator')
 
 
+const storingValidation = [
+    body('categorieName').notEmpty()
+]
 
 
 const index = async (req, res) => {
-    res.json({
-        message: "hey"
-    });
+    try {
+        const categories = await Categorie.find({ deletedAt: null});
+
+        res.json(categories);
+    } catch (error) {
+        res.json(internalError());
+    }
+
 }
 
 
+const store = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty())
+        {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const existingDeletedCategorie = await Categorie.findOne({ categorieName: req.body.categorieName, deletedAt:{$ne: null}})
+
+        if(existingDeletedCategorie){
+            await existingDeletedCategorie.deleteOne();
+        }
+
+        let newCategorie = new Categorie({
+            categorieName: req.body.categorieName,
+            subcategorie: [],
+            createdBy: req.user._id,
+        })
+
+        await newCategorie.save();
+
+        res.json(newCategorie);
+    } catch (error) {
+        res.json(internalError("", error));
+    }
+
+}
 
 
-module.exports = { index }
+module.exports = { index, store, storingValidation }

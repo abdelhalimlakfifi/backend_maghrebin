5a08ec5b6185460cb9bd3e18f59dc4cf67fb29d7
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-10-27 12:22:04 +0100

    make middleware for permissions

--- middleware/backoffice/categorie.middleware.js
+++ middleware/backoffice/categorie.middleware.js
@@ -1,23 +1,21 @@
 const User = require('../../models/user.model');
-const {checkPermissions} = require('../../utils/checkPermissions')
 
-const categorieRead = async (req, res, next) => {
+
+const categorieMiddleware = (permissionNeeded) => async (req, res, next) => {
 
     const userPermissions = req.user.role.permissions;
 
-    const permission = userPermissions.find(permission =>  permission.label === 'categorie-read')
+    const permission = userPermissions.find(permission =>  permission.label === permissionNeeded)
 
     if(permission){
-        next()
+        return next();
+    }else{
+        return res.status(403).json({ message: 'Permission denied' });
     }
-    return res.status(403).json({ message: 'Permission denied' });
-
-    
-    
 }
 
 
 
 module.exports = {
-    categorieRead
+    categorieMiddleware
 }

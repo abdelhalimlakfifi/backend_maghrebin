ea3906b6f848acf4d1f70cc298a8f52a55f76902
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-10-28 22:53:07 +0100

    make type model

---
+++ models/type.model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+
+const typeSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true
+        },
+
+        active: {
+            type: Boolean,
+            require:true,
+            default: true 
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        },
+        updatedBy:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'User',
+            default : null
+        },
+        deletedAt:{
+            type : Date ,
+            default : null
+        },
+        deletedBy:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'User',
+            default : null
+        }
+    },{
+        timestamps: true
+    }
+    
+)
+
+
+typeSchema.methods.softDelete = async function(userid){
+    try {
+        this.deletedAt = new Date();
+        this.deletedBy = userid;
+        await  this.save();
+    } catch (error) {
+        console.error ('Error while removing the role:', error);
+        throw error;
+    }
+}
+
+
+const Type = mongoose.model('Type', typeSchema,'types')
+module.exports = Type;

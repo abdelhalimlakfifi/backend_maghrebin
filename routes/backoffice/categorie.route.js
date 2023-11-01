b2543593afdb21c2ee93dce44b572e7dd8f10134
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-01 14:53:59 +0100

    adding permission midleware

--- routes/backoffice/categorie.route.js
+++ routes/backoffice/categorie.route.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const categorieController = require('../../controllers/backoffice/categorie.controller');
 const { authenticateToken } = require('../../middleware/authMiddleware');
-const { categorieMiddleware } = require('../../middleware/backoffice/categorie.middleware');
+const {permissionMiddleware} = require('../../middleware/backoffice/permissions.middleware');
 
-router.get('/', authenticateToken, categorieMiddleware('categorie-read'), categorieController.index);
-router.post('/store', authenticateToken, categorieMiddleware('categorie-add'), categorieController.storingValidation , categorieController.store);
-router.get('/getone/:categoriename',authenticateToken, categorieMiddleware('categorie-read'), categorieController.getOne);
-router.get('/search/:search',authenticateToken, categorieMiddleware('categorie-read'), categorieController.search);
-router.put('/update/:id',authenticateToken, categorieMiddleware('categorie-edit'),categorieController.storingValidation, categorieController.update);
-router.delete('/delete/:identifier',authenticateToken, categorieMiddleware('categorie-edit'), categorieController.destroy)
+
+router.get('/', authenticateToken, permissionMiddleware('categorie-read'), categorieController.index);
+router.post('/store', authenticateToken, permissionMiddleware('categorie-add'), categorieController.storingValidation , categorieController.store);
+router.get('/getone/:categoriename',authenticateToken, permissionMiddleware('categorie-read'), categorieController.getOne);
+router.get('/search/:search',authenticateToken, permissionMiddleware('categorie-read'), categorieController.search);
+router.put('/update/:id',authenticateToken, permissionMiddleware('categorie-edit'),categorieController.storingValidation, categorieController.update);
+router.delete('/delete/:identifier',authenticateToken, permissionMiddleware('categorie-edit'), categorieController.destroy)
+
 module.exports=router;

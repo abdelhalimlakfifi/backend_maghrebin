083a50c2b651ccd27a8fda26e90c4e11d2cba1f4
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-01 14:16:12 +0100

    change model name

--- seeder/permission.seeder.js
+++ seeder/permission.seeder.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Permission = require('../models/permissionModel');
+const Permission = require('../models/permission.model');
 require('dotenv').config();
 const connectDB = require('../config/db');
 

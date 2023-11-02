fc4c41acb61f2024cb9b8e8d4f5a12b3ffb968b4
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-02 09:42:43 +0100

    import authenticate midleware and add the route

--- routes/backoffice/authRoutes.js
+++ routes/backoffice/authRoutes.js
@@ -1,9 +1,10 @@
 // authRoutes.js
 const express = require("express");
 const authController = require("../../controllers/backoffice/authController");
-
+const { authenticateToken } = require('../../middleware/authMiddleware');
 const router = express.Router();
 
 router.post("/login", authController.validateLogin, authController.loginUser);
+router.get('/checkAuth', authenticateToken, authController.checkAuth)
 
 module.exports = router;

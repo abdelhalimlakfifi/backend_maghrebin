13aa79946e7fba644541fbcbeb6db2f355758ca8
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-02 09:32:36 +0100

    make route

---
+++ routes/backoffice/subcategorie.route.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const router = express.Router();
+const subCategorieController = require('../../controllers/backoffice/subcategorie.controller');
+const { authenticateToken } = require('../../middleware/authMiddleware');
+const {permissionMiddleware} = require('../../middleware/backoffice/permissions.middleware');
+
+
+router.get('/', authenticateToken, permissionMiddleware('sub-categorie-read'), subCategorieController.index);
+router.get('/getone/:name',authenticateToken, permissionMiddleware('sub-categorie-read'), subCategorieController.getOne)
+router.post('/store', authenticateToken, permissionMiddleware('sub-categorie-add'), subCategorieController.storingValidation, subCategorieController.store);
+router.put('/update/:id', authenticateToken, permissionMiddleware('sub-categorie-edit'), subCategorieController.storingValidation, subCategorieController.update);
+router.delete('/delete/:identifier',authenticateToken, permissionMiddleware('sub-categorie-delete'), subCategorieController.storingValidation, subCategorieController.destroy);
+
+
+
+
+module.exports = router;

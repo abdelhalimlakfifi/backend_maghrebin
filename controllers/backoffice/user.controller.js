f592db73c7ffe07a4a2cb44601a50d8e5817ce9b
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-10 14:33:20 +0100

    building user crud

--- controllers/backoffice/user.controller.js
+++ controllers/backoffice/user.controller.js
@@ -1,3 +1,4 @@
+
 const User = require('../../models/user.model');
 const { internalError } = require('../../utils/500'); // Import a custom internalError function.
 const { body, validationResult } = require('express-validator'); // Import express-validator for input validation.
@@ -15,7 +16,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
 
 
@@ -47,58 +48,49 @@ const index = async (req, res) => {
         res.json(internalError("", error))
     }
 }
+const upload = multer({ storage }).single('profile_picture'); // Assuming 'file' is the name attribute in the form
 
 
 const store = async (req, res) => {
-    
-
-    const x = await upload.single('profile_picture');
-    console.log(req.body);
-    console.log(x);
-    res.json({
-        test: "test"
-    });
-    return
-   
-   
-   
-   
-   
     try {
+
+
+        console.log(req.body);
+      upload(req, res, async (err) => {
+        if (err instanceof multer.MulterError) {
+          // A Multer error occurred when uploading
+          return res.status(500).json({ error: err.message });
+        } else if (err) {
+          // An unknown error occurred when uploading
+          return res.status(500).json({ error: 'An unknown error occurred' });
+        }
+  
+        console.log(req.body); // Access other form fields (JSON data)
+        
+        // Your file information is available in req.file
+        const uploadedFile = req.file;
+  
+        // Continue with the rest of your controller logic
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+          return res.status(400).json({ errors: errors.array() });
         }
-
-        const foundedEmail = await User.findOne({ email:req.body.email });
-        if(foundedEmail){
-            if(foundedEmail.deletedAt != null){
-                await foundedEmail.deleteOne();
-            }else{
-                return res.status(409).json({
-                    status: 409,
-                    message: "Email Already exist"
-                });
-            }
-        }
-
-        const foundedUsername = await User.findOne({ username: req.body.username });
-        if(foundedUsername){
-            if(foundedUsername.deletedAt != null){
-                await foundedUsername.deleteOne();
-            }else{
-                return res.status(409).json({
-                    status: 409,
-                    message: "Username Already exist"
-                });
-            }
-        }
-
-        
+  
+        // The rest of your logic...
+  
+        // Respond to the client
+        res.json({
+          message: 'File uploaded successfully',
+          data: req.body,
+          file: uploadedFile,
+        });
+      });
     } catch (error) {
-        
+      // Handle errors
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-}
+  };
 
 
 const getOne = async (req, res) => {
@@ -123,4 +115,4 @@ const destroy = async( req, res) => {
 }
 
 
-module.exports = { index, store, getOne, search, update, destroy, storingValidation };
+module.exports = { index, store, getOne, search, update, destroy,upload, storingValidation };

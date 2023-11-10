3b3ab0ca9261413824946a62934413e80a8ef3d6
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-10 09:55:11 +0100

    create user

---
+++ controllers/backoffice/user.controller.js
@@ -0,0 +1,103 @@
+const User = require('../../models/user.model');
+const { internalError } = require('../../utils/500'); // Import a custom internalError function.
+const { body, validationResult } = require('express-validator'); // Import express-validator for input validation.
+const mongoose = require('mongoose'); // Import mongoose for working with MongoDB.
+
+
+
+const storingValidation = [
+    body('first_name').notEmpty(),
+    body('last_name').notEmpty(),
+    body('username').notEmpty(),
+    body('password').notEmpty(),
+    body('email').notEmpty(),
+    body('role').notEmpty(),
+    body('profile_picture')
+];
+
+const index = async (req, res) => {
+    try {
+
+        const users = await User.find({ deletedAt: null }).populate({
+            path: 'role',
+            populate: {
+                path: 'permissions',
+                model:'Permission',
+                select: 'label'
+            }
+        })
+        .exec();
+        res.json(users);
+    } catch (error) {
+        res.json(internalError("", error))
+    }
+}
+
+
+const store = async (req, res) => {
+    
+    console.log(req.body);
+    res.json({
+        test: "test"
+    });
+    return
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const foundedEmail = await User.findOne({ email:req.body.email });
+        if(foundedEmail){
+            if(foundedEmail.deletedAt != null){
+                await foundedEmail.deleteOne();
+            }else{
+                return res.status(409).json({
+                    status: 409,
+                    message: "Email Already exist"
+                });
+            }
+        }
+
+        const foundedUsername = await User.findOne({ username: req.body.username });
+        if(foundedUsername){
+            if(foundedUsername.deletedAt != null){
+                await foundedUsername.deleteOne();
+            }else{
+                return res.status(409).json({
+                    status: 409,
+                    message: "Username Already exist"
+                });
+            }
+        }
+
+        
+    } catch (error) {
+        
+    }
+}
+
+
+const getOne = async (req, res) => {
+
+}
+
+
+
+const search = async( req, res) => {
+
+}
+
+
+const update = async( req, res) => {
+    
+}
+
+
+
+const destroy = async( req, res) => {
+    
+}
+
+
+module.exports = { index, store, getOne, search, update, destroy, storingValidation };

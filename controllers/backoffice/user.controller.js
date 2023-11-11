9ed0086f8d9ef8a872ed1b35726e76dfb2dd9b15
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-11-11 18:30:30 +0100

    create getOne Cotroller

--- controllers/backoffice/user.controller.js
+++ controllers/backoffice/user.controller.js
@@ -15,29 +15,29 @@ const store = async (req, res) => {
         const uploadedFile = await uploadFileFunction(req, res, 'profile_picture');
         await Promise.all([
             check('first_name')
-                .notEmpty()
-                .withMessage('First name is required')
-                .run(req),
+            .notEmpty()
+            .withMessage('First name is required')
+            .run(req),
             check('last_name')
-                .notEmpty()
-                .withMessage('Last name is required')
-                .run(req),
+            .notEmpty()
+            .withMessage('Last name is required')
+            .run(req),
             check('username')
-                .notEmpty()
-                .withMessage('username is required')
-                .run(req),
+            .notEmpty()
+            .withMessage('username is required')
+            .run(req),
             check('email')
-                .isEmail()
-                .withMessage('email is required')
-                .run(req),
+            .isEmail()
+            .withMessage('email is required')
+            .run(req),
             check('role')
-                .notEmpty()
-                .withMessage('role is required')
-                .run(req),
+            .notEmpty()
+            .withMessage('role is required')
+            .run(req),
             check('password')
-                .isLength({ min: 6 })
-                .withMessage('Password must be at least 6 characters long')
-                .run(req),
+            .isLength({ min: 6 })
+            .withMessage('Password must be at least 6 characters long')
+            .run(req),
         ]);
 
 
@@ -86,10 +86,10 @@ const store = async (req, res) => {
         {
             imagePath = uploadedFile.destination + uploadedFile.originalname
         }
-        
-        
 
-        
+
+
+
 
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
         const user = new User({
@@ -102,7 +102,7 @@ const store = async (req, res) => {
             profile_picture: imagePath,
             createdBy: req.user._id,
         })
-        
+
 
         await user.save();
         return res.status(200).json({
@@ -121,23 +121,61 @@ const store = async (req, res) => {
 
 const getOne = async (req, res) => {
 
+    try {
+        // Extract id from request parameters
+        const {
+            id
+        } = req.params;
+
+        // Validate id
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                error: 'Invalid user ID'
+            });
+        }
+
+        // Find user by ID
+        const user = await User.findById(id)
+            .populate('role', 'name permissions');
+
+        // Check if user exists
+        if (!user) {
+            return res.status(404).json({
+                error: 'User not found'
+            });
+        }
+
+        // Return user
+        return res.status(200).json({
+            user
+        });
+    } catch (error) {
+        // Handle errors
+        return res.status(500).json({
+            error: 'Something went wrong'
+        });
+    }
+
 }
 
 
 
-const search = async( req, res) => {
+const search = async (req, res) => {
+    const {
+        id
+    } = req.params;
 
 }
 
 
-const update = async( req, res) => {
-    
+const update = async (req, res) => {
+
 }
 
 
 
-const destroy = async( req, res) => {
-    
+const destroy = async (req, res) => {
+
 }
 
 
@@ -146,19 +184,26 @@ const index = async (req, res) => {
     try {
 
         const users = await User.find()
-        .populate({
-            path: 'role',
-            populate: {
-                path: 'permissions',
-                model:'Permission',
-                select: 'label'
-            }
-        })
-        .exec();
+            .populate({
+                path: 'role',
+                populate: {
+                    path: 'permissions',
+                    model: 'Permission',
+                    select: 'label'
+                }
+            })
+            .exec();
         res.json(users);
     } catch (error) {
         res.json(internalError("", error))
     }
 }
 
-module.exports = { index, store, getOne, search, update, destroy };
+module.exports = {
+    index,
+    store,
+    getOne,
+    search,
+    update,
+    destroy
+};

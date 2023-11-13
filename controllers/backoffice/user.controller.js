1c94d4ae2f3076d4dff4a72f4c7d8f3e57622b1c
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-13 15:21:13 +0100

    add images

--- controllers/backoffice/user.controller.js
+++ controllers/backoffice/user.controller.js
@@ -1,7 +1,11 @@
-
 const User = require('../../models/user.model');
-const { internalError } = require('../../utils/500'); // Import a custom internalError function.
-const { body, validationResult } = require('express-validator'); // Import express-validator for input validation.
+const {
+    internalError
+} = require('../../utils/500'); // Import a custom internalError function.
+const {
+    body,
+    validationResult
+} = require('express-validator'); // Import express-validator for input validation.
 const mongoose = require('mongoose'); // Import mongoose for working with MongoDB.
 const multer = require('multer');
 
@@ -12,7 +16,7 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now()+'-'+file.originalname);
+        cb(null, Date.now() + '-' + file.originalname);
     }
 });
 
@@ -34,21 +38,23 @@ const index = async (req, res) => {
     try {
 
         const users = await User.find()
-        .populate({
-            path: 'role',
-            populate: {
-                path: 'permissions',
-                model:'Permission',
-                select: 'label'
-            }
-        })
-        .exec();
+            .populate({
+                path: 'role',
+                populate: {
+                    path: 'permissions',
+                    model: 'Permission',
+                    select: 'label'
+                }
+            })
+            .exec();
         res.json(users);
     } catch (error) {
         res.json(internalError("", error))
     }
 }
-const upload = multer({ storage }).single('profile_picture'); // Assuming 'file' is the name attribute in the form
+const upload = multer({
+    storage
+}).single('profile_picture'); // Assuming 'file' is the name attribute in the form
 
 
 const store = async (req, res) => {
@@ -56,41 +62,49 @@ const store = async (req, res) => {
 
 
         console.log(req.body);
-      upload(req, res, async (err) => {
-        if (err instanceof multer.MulterError) {
-          // A Multer error occurred when uploading
-          return res.status(500).json({ error: err.message });
-        } else if (err) {
-          // An unknown error occurred when uploading
-          return res.status(500).json({ error: 'An unknown error occurred' });
-        }
-  
-        console.log(req.body); // Access other form fields (JSON data)
-        
-        // Your file information is available in req.file
-        const uploadedFile = req.file;
-  
-        // Continue with the rest of your controller logic
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-  
-        // The rest of your logic...
-  
-        // Respond to the client
-        res.json({
-          message: 'File uploaded successfully',
-          data: req.body,
-          file: uploadedFile,
+        upload(req, res, async (err) => {
+            if (err instanceof multer.MulterError) {
+                // A Multer error occurred when uploading
+                return res.status(500).json({
+                    error: err.message
+                });
+            } else if (err) {
+                // An unknown error occurred when uploading
+                return res.status(500).json({
+                    error: 'An unknown error occurred'
+                });
+            }
+
+            console.log(req.body); // Access other form fields (JSON data)
+
+            // Your file information is available in req.file
+            const uploadedFile = req.file;
+
+            // Continue with the rest of your controller logic
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array()
+                });
+            }
+
+            // The rest of your logic...
+
+            // Respond to the client
+            res.json({
+                message: 'File uploaded successfully',
+                data: req.body,
+                file: uploadedFile,
+            });
         });
-      });
     } catch (error) {
-      // Handle errors
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        // Handle errors
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal Server Error'
+        });
     }
-  };
+};
 
 
 const getOne = async (req, res) => {
@@ -99,20 +113,29 @@ const getOne = async (req, res) => {
 
 
 
-const search = async( req, res) => {
+const search = async (req, res) => {
 
 }
 
 
-const update = async( req, res) => {
-    
+const update = async (req, res) => {
+
 }
 
 
 
-const destroy = async( req, res) => {
-    
+const destroy = async (req, res) => {
+
 }
 
 
-module.exports = { index, store, getOne, search, update, destroy,upload, storingValidation };
+module.exports = {
+    index,
+    store,
+    getOne,
+    search,
+    update,
+    destroy,
+    upload,
+    storingValidation
+};

4ec22b7e24717dc5d403a99ccd99cd317fbeebb1
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-15 12:05:42 +0100

    make route for index and route for delete tag

--- routes/backoffice/tag.route.js
+++ routes/backoffice/tag.route.js
@@ -6,9 +6,10 @@ const tagController = require('../../controllers/backoffice/tag.controller');
 
 
 
-
+router.get('/', authenticateToken, permissionMiddleware('tag-read'), tagController.index)
 router.post('/store', authenticateToken, permissionMiddleware('tag-add'), tagController.storingValidation, tagController.store);
 router.put('/update/:id', authenticateToken, permissionMiddleware('tag-edit'), tagController.update)
+router.delete('/delete/:id', authenticateToken, permissionMiddleware('tag-delete'), tagController.remove);
 
 
 module.exports = router;

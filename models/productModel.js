1e35a6faf181f17759b57c4f5b2121cc8a809de5
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-11-15 14:29:59 +0100

    making model

--- models/productModel.js
+++ models/productModel.js
@@ -1,24 +1,55 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  id: { type: String, required: true },
-  ref: { type: String, required: true },
-  images: [
-    {
-      image_path: { type: String, required: true },
-      main: { type: Boolean, required: true },
+
+const productSchema = new mongoose.Schema({
+    ref: {
+        type: String,
+        required: true
     },
-  ],
-  deleted: { type: Boolean, default: false }, 
-  deletedAt: { type: Date, default: null }, 
+    images: [{
+        image_path: {
+            type: String,
+            required: true
+        },
+        main: {
+            type: Boolean,
+            required: true
+        },
+        secondary: {
+            type: Boolean,
+            required: true
+        }
+    }],
+    title: {
+        type: String,
+        required: true
+    },
+    short_description: {},
+    long_description: {},
+    price: {},
+    short_description: {},
+    reviews: [{}],
+
+    createdBy: {},
+    updatedBy: {},
+    deletedBy: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: {
+        type: Date,
+        default: null
+    },
+},
+{
+    timestamps: true
 });
 
 productSchema.pre('save', function (next) {
-  if (this.deleted) {
-    this.deletedAt = new Date(); 
-  }
-  next();
+    if (this.deleted) {
+        this.deletedAt = new Date();
+    }
+    next();
 });
 
 const Product = mongoose.model("Product", productSchema);

ce09beb3580b152982a41e593754560d4b0fe067
Mohammed El Mouden <mohamed55mouden@gmail.com>
2023-12-11 14:47:03 +0100

    product image

--- models/productimage.model.js
+++ models/productimage.model.js
@@ -1,40 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const productImage = new mongoose.Schema(
-  {
-    path: {
-      type: String,
-      required: true,
-    },
-    main: {
-      type: Boolean,
-      required: true,
-    },
-    secondary: {
-      type: Boolean,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+    {
+        path: {
+            type:String,
+            require: true
+        },
+        main: {
+            type:Boolean,
+            required: true
+        },
+        secondary: {
+            type: Boolean,
+            required: true
+        },
+    },{
+        timestamps: true
+    }
+)
 
-productImage.methods.softDelete = async function (userid) {
-  try {
-    this.deletedAt = new Date();
-    this.deletedBy = userid;
-    await this.save();
-  } catch (error) {
-    console.error("Error while removing the role:", error);
-    throw error;
-  }
-};
 
-const ProductImage = mongoose.model(
-  "ProductImage",
-  productImage,
-  "product_images"
-);
+productImage.methods.softDelete = async function(userid){
+    try {
+        this.deletedAt = new Date();
+        this.deletedBy = userid;
+        await  this.save();
+    } catch (error) {
+        console.error('Error while removing the role:', error);
+        throw error;
+    }
+}
+
+
+const ProductImage = mongoose.model('ProductImage', productImage, "product_images");
 
 module.exports = ProductImage;

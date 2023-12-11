53e7a903122405b192a0aac07f558bc527f21afc
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-12-11 22:05:02 +0100

    change the path of my auth middleware

--- routes/backoffice/roles.route.js
+++ routes/backoffice/roles.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const roleController = require('../../controllers/backoffice/roles.controller');
-const { authenticateToken } = require('../../middleware/authMiddleware');
+const { authenticateToken } = require('../../middleware/backoffice/authMiddleware');
 const {permissionMiddleware} = require('../../middleware/backoffice/permissions.middleware');
 
 // get

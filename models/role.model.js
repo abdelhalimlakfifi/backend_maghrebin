5e86336d5a1b784bd60bcd6298c31863643fc998
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-12-11 21:58:50 +0100

    add CDU By and At

--- models/role.model.js
+++ models/role.model.js
@@ -1,42 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const roleSchema = new mongoose.Schema(
-    {
-        role: { 
-            type: String, 
-            required: true,
-            unique: true
-        },
-        permissions: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Permission'
-            }
-        ],
+  {
+    role: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    permissions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Permission",
+      },
+    ],
 
-        deletedAt:{
-            type : Date ,
-            default : null
-
-        }
-    },{
-        timestamps: true
-    }
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    deletedAt: {
+      type: Date,
+      default: null,
+    },
+    deletedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-
 roleSchema.methods.softDelete = async function () {
-    try {
-        // Set the 'deletedAt' field to the current date
-        this.deletedAt = new Date();
-        await this.save(); // Save the updated document
-    } catch (error) {
-        // Handle any errors that occur during the process
-        console.error('Error while removing the role:', error);
-        throw error;
-    }
-}
+  try {
+    // Set the 'deletedAt' field to the current date
+    this.deletedAt = new Date();
+    await this.save(); // Save the updated document
+  } catch (error) {
+    // Handle any errors that occur during the process
+    console.error("Error while removing the role:", error);
+    throw error;
+  }
+};
 
-
-const Role = mongoose.model('Role', roleSchema, 'roles')
+const Role = mongoose.model("Role", roleSchema, "roles");
 module.exports = Role;

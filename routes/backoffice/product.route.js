840bf53d4d31d3a9479f00c3b9abda7a4529a5fa
Mohammed El Mouden <mohamed55mouden@gmail.com>
2023-12-11 14:45:49 +0100

    merge seder product with main

--- routes/backoffice/product.route.js
+++ routes/backoffice/product.route.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../../controllers/backoffice/product.controller');
+const productController = require("../../controllers/backoffice/product.controller");
 
 // get
 router.get("/", productController.getAll);
 
-
 // create
-router.get("/create", productController.create)
+router.get("/create", productController.create);
 // get one
-router.get('/getone/:id', productController.getOne);
+router.get("/getone/:id", productController.getOne);
+
+router.get("/getAll", productController.getAll);
 
 // Post
-router.post('/store', productController.store);
+router.post("/store", productController.store);
 
 // search
-router.get('/:search', productController.search);
+router.get("/:search", productController.search);
 
 // update
-router.put('/update/:id', productController.update);
+router.put("/update/:id", productController.update);
 
 // delete
-router.delete('/delete/:id', productController.remove);
+router.delete("/delete/:id", productController.remove);
 
-
-module.exports=router;
+module.exports = router;

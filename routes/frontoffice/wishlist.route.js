7cb769769ec619691d7637a98284c0b8c908a1d9
abdelhalim LAKFIFI <62565296+abdelhalimlakfifi@users.noreply.github.com>
2023-12-13 05:47:40 +0100

    refactore

--- routes/frontoffice/wishlist.route.js
+++ routes/frontoffice/wishlist.route.js
@@ -1,22 +1,15 @@
 const express = require("express");
-const passport = require("passport");
-const { authenticateToken } = require("../../middleware/authMiddleware");
-const {
-  getCustomerWishlist,
-  postCustomerWishlist,
-  deleteCustomerProduct,
-} = require("../../controllers/frontoffice/wishlist.controller");
-
-const router = express.Router();
-
-// --------------------Routes for customers """DONT FORGET authenticateToken('customer-jwt'),"""
+const { getCustomerWishlist, postCustomerWishlist, deleteCustomerProduct } = require("../../controllers/frontoffice/wishList.controller");
+const wishListRoutes = express.Router();
 // Get the wish list of customers
-router.get("/customer/:id", getCustomerWishlist);
+wishListRoutes.get("/:id", getCustomerWishlist);
 
 // Post the wish list of customers
-router.post("/customer/:customerId/wishlist/product/:productId", postCustomerWishlist);
+wishListRoutes.post(
+  "/:customerId/wishlist/product/:productId",
+  postCustomerWishlist
+);
 
 // Delete Product from the wishlist of customers
-router.delete("/customer/:customerId/wishlist/product", deleteCustomerProduct);
-
-module.exports = router;
+wishListRoutes.delete("/:customerId/wishlist/product", deleteCustomerProduct);
+module.exports = wishListRoutes;
